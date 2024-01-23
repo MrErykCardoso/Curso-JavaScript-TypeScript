763999880f62f5c6d1a467e9f906740faf0d4b95
MrErykCardoso <mr.erykcardoso@gmail.com>
2024-01-23 14:07:38 -0300

    update

---
+++ Pratica 1/assets/script.js
@@ -0,0 +1,66 @@
+///////////////// Function /////////////////////
+
+/*function saudacao(name) {
+    return `Bom dia ${name}`;
+
+}
+
+const varia = saudacao('Marcelo');
+console.log(varia);
+
+function soma(x = 2, y = 2) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma(5, 5);
+console.log(resultado);*/
+
+/*const raiz = function (n) {
+    return resultado = n ** 0.5;
+}
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));*/
+
+///////////////// object /////////////////
+
+/*const pessoa1 = {
+    nome: 'marcelo',
+    sobrenome: 'cardoso',
+    idade: 23
+}
+
+const pessoa2 = {
+    nome: 'dara',
+    sobrenome: 'cardoso',
+    idade: 24
+}
+
+console.log(pessoa1.nome, pessoa2.sobrenome);*/
+
+/*function criaPessoa(nome, sobrenome, idade) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade,
+
+        fala() {
+            console.log(`Minha idade é ${this.idade}`);
+        },
+
+        passaTempo() {
+            this.idade++;
+        }
+    };
+}
+
+const pessoa1 = criaPessoa('Marcelo', 'Cardoso', 23);
+const pessoa2 = criaPessoa('Milenna', 'Teixeira', 23);
+
+pessoa1.fala();
+pessoa1.passaTempo();
+pessoa1.fala();
+pessoa2.fala();
+pessoa2.passaTempo();
+pessoa2.fala();*/

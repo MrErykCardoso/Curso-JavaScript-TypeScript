02750fbd94f812b87b011278a68aeb6cd057dd96
MrErykCardoso <mr.erykcardoso@gmail.com>
2024-02-12 14:18:39 -0300

    update

--- Pratica 1/assets/script.js
+++ Pratica 1/assets/script.js
@@ -1,3 +1,79 @@
+/// Exercício teste de IMC ///
+
+function imcMain() {
+    const form = document.querySelector('.form2');
+    const result = document.querySelector('.resultExe2');
+
+    function getEvent(event) {
+        event.preventDefault();
+        const inputPeso = form.querySelector('.weight');
+        const inputAltura = form.querySelector('.height');
+
+        const peso = Number(inputPeso.value);
+        const altura = Number(inputAltura.value);
+
+        const imc = peso / altura ** 2;
+
+        if (imc < 18.5) {
+            result.innerHTML = `<p class="sucessExe2">Abaixo do peso. Seu IMC: ${imc.toFixed(2)}.</p>`;
+        }
+        else if (imc >= 18.5 && imc <= 24.9) {
+            result.innerHTML = `<p class="sucessExe2">Peso normal. Seu IMC: ${imc.toFixed(2)}.</p>`;
+        }
+        else if (imc >= 25 && imc <= 29.9) {
+            result.innerHTML = `<p class="sucessExe2">Sobrepeso. Seu IMC: ${imc.toFixed(2)}.</p>`;
+        }
+        else if (imc >= 30 && imc <= 34.9) {
+            result.innerHTML = `<p class="sucessExe2">Obesidade de grau 1. Seu IMC: ${imc.toFixed(2)}.</p>`;
+        }
+        else if (imc >= 35 && imc <= 39.9) {
+            result.innerHTML = `<p class="sucessExe2">Obesidade de grau 2. Seu IMC: ${imc.toFixed(2)}.</p>`;
+        }
+        else if (imc > 40) {
+            result.innerHTML = `<p class="sucessExe2">Obesidade de grau 3. Seu IMC: ${imc.toFixed(2)}.</p>`;
+        } else {
+            result.innerHTML = `<p class="errorExe2">Erro! Valor inválido.</p>`;
+        }
+    }
+    form.addEventListener('submit', getEvent);
+}
+imcMain();
+
+/// Exercício de função array objeto ////
+
+function meuEscopo() {
+    const form = document.querySelector('.form1');
+    const result = document.querySelector('.result');
+
+    const pessoas = [];
+
+    function getEvent(event) {
+        event.preventDefault();
+        const name = form.querySelector('.name');
+        const lastname = form.querySelector('.sob');
+        const height = form.querySelector('.alt');
+        const weight = form.querySelector('.peso');
+
+        console.log('check (1)!');
+
+        pessoas.push({
+            name: name.value,
+            lastname: lastname.value,
+            height: height.value,
+            weight: weight.value
+        })
+
+        console.log('check (2)!');
+
+        console.log(pessoas);
+
+        result.innerHTML += `<p>${name.value}, ${lastname.value}, ${height.value}, ${weight.value}</p>`;
+
+    }
+    form.addEventListener('submit', getEvent);
+}
+meuEscopo();
+
 ///////////////// Function /////////////////////
 
 /*function saudacao(name) {
